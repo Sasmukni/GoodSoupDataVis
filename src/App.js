1fe49096bff236eb5ac82741cb06bad728de72d7
Sasmukni <samukni@gmail.com>
2024-10-27 19:23:41 +0100

    Working version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
-import About from "./routes/About";
-import Careers from "./routes/Careers";
-import Home from "./routes/Home";
+import Home from "./routes/home";
 import Navbar from './navbar';
 
 function App() {
@@ -11,29 +9,7 @@ function App() {
     <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/careers" element={<Careers />} />
       </Routes>
-    <div className="App">
-      <header className="App-header">
-        <h1>Final Project Name</h1>
-        <h2>Data Visualization - <b>Good Soup</b></h2>
-        <h3>Members:</h3>  
-        <ul>
-          <li>Alluto Enrico</li>
-          <li>Capani Samuele</li>
-          <li>Fuciarelli Laura</li>
-          <li>Siddi Yryskeldi</li>
-        </ul>
-
-        <h2>Project Description</h2>
-        <p>To Be Done</p>
-
-        <h2>Assignment 1</h2>
-        <h3>CO2 Emissions</h3>
-        <h4>Comparing Categories</h4>
-      </header>
-    </div>
     </>
   );
 }
