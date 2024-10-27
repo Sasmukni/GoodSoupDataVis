8f90783127a782b11ed1755485591a824672ffc7
Sasmukni <samukni@gmail.com>
2024-10-27 18:53:37 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import About from "./routes/About";
+import Careers from "./routes/Careers";
+import Home from "./routes/Home";
+import Navbar from './Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/careers" element={<Careers />} />
+      </Routes>
     <div className="App">
       <header className="App-header">
         <h1>Final Project Name</h1>
@@ -22,6 +34,7 @@ function App() {
         <h4>Comparing Categories</h4>
       </header>
     </div>
+    </>
   );
 }
 
